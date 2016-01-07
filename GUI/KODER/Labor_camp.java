5bda141b176886ba01fa01dd1bf28968abb122a3
TC06 <Cetin_tolga06@hotmail.com>
2016-01-07 22:19:17 +0100

    Lavet Labor camp klasse
    Klassen er færdig

--- GUI/KODER/Labor_camp.java
+++ GUI/KODER/Labor_camp.java
@@ -18,6 +18,7 @@ public class Labor_camp extends Ownable{
         D.Throw();
         int s = D.sum();
         this.rent=rent;
+<<<<<<< Updated upstream
         FieldHandler f = new FieldHandler();
         Ownable O = (Ownable) f.Field[12];
         Ownable O2 = (Ownable) f.Field[28];
@@ -25,5 +26,41 @@ public class Labor_camp extends Ownable{
             this.rent = s*rent*2;
             
         }
+=======
+        FieldHandler f=new FieldHandler();
+        Dicecup D = new Dicecup();
+        D.Throw();
+        int sum = D.sum();
+        Ownable O = (Ownable) f.Field[12];
+        Ownable O2 = (Ownable) f.Field[28];
+        if(O.getOwner().name()==O2.getOwner().name()){
+            
+            this.rent=sum*rent*2;
+            
+        }
+        else if(O.getOwner().name() != O2.getOwner().name()){
+            
+            this.rent=sum*rent;
+            
+        }
+        else if(O.getOwner().name()==null || O2.getOwner().name()==null){
+            this.rent=0;
+           /* String V = GUI.getUserButtonPressed("Vil du købe grunden til 3000?", "Ja","Nej"){
+                if(V=="Ja"){
+                    
+                    
+                }
+                else if(V=="Nej"){
+                    continue;
+                }
+                
+            }*/
+        }
+        
+    }
+
+    public void landOnField(Players player) {
+        
+>>>>>>> Stashed changes
     }
 }
