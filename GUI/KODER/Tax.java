9085a904da4795e85c538c94b25fd4d41b3c08bb
samilesma <samilesma@gmail.com>
2016-01-08 17:02:16 +0100

    Tax felt

--- GUI/KODER/Tax.java
+++ GUI/KODER/Tax.java
@@ -12,6 +12,25 @@ public class Tax extends Fields{ // forlænger klasse med en anden klasse
     
     @Override
     public void landOnField(Players player){
-        
+        FieldHandler f= new FieldHandler();
+        int EjendomSum = 0; 
+        for(int i = 0; i<f.Field.length; i++){
+            Ownable O = (Ownable) f.Field[i];
+            if(O.getOwner().name()==player.name()){
+                EjendomSum +=O.getPrice();
+            }
+        }
+        if(player.getPosition()==38){
+            this.tax=2000;
+        }
+        else{
+            String b = GUI.getUserButtonPressed("Vil du betale 4000 eller 10%", "Betal 4000", "Betal 10%");
+            if(b=="Betal 4000"){
+                this.tax=4000;
+            }
+            else{
+                this.tax=(player.getMoney()+EjendomSum)/10;
+            }
+        }
     }
 }
