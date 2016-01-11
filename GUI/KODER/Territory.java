d0f836ab99ffa0c687200ae9ac9a3f4710059b3f
Mikfor <Mik_f@outlook.dk>
2016-01-11 13:46:31 +0100

    Ændret i 3 klasser

--- GUI/KODER/Territory.java
+++ GUI/KODER/Territory.java
@@ -2,10 +2,18 @@ package KODER;
 
 public class Territory extends Ownable{
     private int rent;
+    private String colour;
     
-    public Territory(String name,int price,int rent, int IH, int IIH, int IIIH,int IVH,int HO,int BH){
+    public Territory(String name,String Colour,int price,int rent, int IH, int IIH, int IIIH,int IVH,int HO,int BH){
         super(name,price);
         this.setRent(rent);
+        this.setColour(Colour);
+    }
+    public void setColour(String colour){
+        this.colour=colour;
+    }
+    public String getColour(){
+        return this.colour;
     }
     
     @Override
---
+++ GUI/KODER/TerritorysController.java
@@ -0,0 +1,11 @@
+package KODER;
+
+public class TerritorysController {
+    private boolean[] House= {false,false,false,false,false};
+    
+    
+    public void BuyH(Players p){
+        
+    }
+
+}
