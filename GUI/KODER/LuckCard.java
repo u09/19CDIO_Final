c5f42093ab44e83296de9d6caee2305fc64a51bf
u09 <u_09@live.dk>
2016-01-11 13:51:04 +0100

    Jeg er igang med at arbejde på Luck, LuckCard og LuckController - NIX PILLE !!

--- GUI/KODER/LuckCard.java
+++ GUI/KODER/LuckCard.java
@@ -1,34 +1,32 @@
 package KODER;
 
 public class LuckCard {
-    private String      text;   //vi opretter variabel til visning af tekst i guien
-    private String      type;   //vi oprretter type så vi ved om der skal rykkes frem/tilbage, betale til banken osv.
-    private int         value;  //Vi opretter værdien som vi skal bruge til at rykke frem/tilbage eller betale/modtage beløb osv
+    private String text;   // variabel til visning af tekst i guien
+    private String type;   // type så vi ved om der skal rykkes frem/tilbage, betale til banken osv.
+    private int value;  // værdien som vi skal bruge til at rykke frem/tilbage eller betale/modtage beløb osv
+    private int luckFieldNum; // variabel til at vide om spilleren skal modtage 4000 kr. ved passering af start
     
-    
-        public LuckCard ( String cardType, String cardText, int cardValue){
-            type  = cardType;
-            text  = cardText;
-            value = cardValue;
-            
-        }
-    
-    
-    
-    public void useCard(Players p){
-        
-        
-        
-        
-        switch (type) {
-            
-            case"move":
-                
-                
-            
-        }
+    public LuckCard(String cardType, String cardText, int cardValue) {
+        type = cardType;
+        text = cardText;
+        value = cardValue;
         
     }
     
-}
-
+    public LuckCard(String text, String type, int value, int luckFieldNum) {
+        this.text = text;
+        this.type = type;
+        this.value = value;
+        this.luckFieldNum = luckFieldNum;
+    }
+    
+    public void useCard(Players p) {
+        // 2, 7, 17, 22, 33, 36
+           String pickCard = GUI.getUserButtonPressed("Vil du prøve lykken?", "Ja", "Nej");
+            if(pickCard.equals("Ja")) {
+                LuckCard card = LuckController.getLuckCard();
+                if(card > )
+            }
+        }
+            
+        }
