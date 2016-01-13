72aa37a4d2e769a58a53bb0dc19ba248a4d414a2
u09 <u_09@live.dk>
2016-01-13 02:50:05 +0100

    Klassediagram og oprettet JUnit klasse

---
+++ GUI/Tests/JUnit.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JUnit {
+    
+    @Test
+    public void test() {
+        assertArrayEquals("Move", "Ryk frem til START", );
+    }
+    
+}
