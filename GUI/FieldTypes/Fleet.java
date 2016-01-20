5ddbb670a0f8f007da3f5858ca6ce06b7c6ba725
TC06 <Cetin_tolga06@hotmail.com>
2016-01-20 21:46:36 +0100

    Kommentarer
    Tilføjet kommentarer til resterende felttype klasser

--- GUI/FieldTypes/Fleet.java
+++ GUI/FieldTypes/Fleet.java
@@ -19,21 +19,24 @@ public class Fleet extends Ownable{
     
     private void setRent(Players p){
         Ownable O[]={(Ownable) FieldHandler.Field[5],(Ownable) FieldHandler.Field[15],(Ownable) FieldHandler.Field[25],(Ownable) FieldHandler.Field[35]};
-        int[] amountp = {0,0,0,0,0,0};
+        int[] amountp = {0,0,0,0,0,0};// Laver et array med hvor mange fleet felter de forskellige spillere ejer
         int n=p.getPosition();
         
+        // Definerer de forskellige felter fleet ligger på
         if(n==6) n=0;
         else if(n==16) n=1;
         else if(n==26) n=2;
         else n=3;
         
+        // Hvis man køber et felt, vil man inde i arrayet få et mere fleet felt, altså "amountp" 
         if(O[0].getOwner()!=null) amountp[O[0].getOwner().getPlayer()]++;
         if(O[1].getOwner()!=null) amountp[O[1].getOwner().getPlayer()]++;
         if(O[2].getOwner()!=null) amountp[O[2].getOwner().getPlayer()]++;
         if(O[3].getOwner()!=null) amountp[O[3].getOwner().getPlayer()]++;
         
+        // Hvis spilleren der lander på det pågældende fleet felt selv ejer det, skal der ikke betales noget
         if(p.getPlayer()==O[n].getOwner().getPlayer()) this.rent=0;
-        else{
+        else{// Jo flere de ejer, jo højere bliver lejen for at lande der 
             if(amountp[O[n].getOwner().getPlayer()]==1) this.rent=500;
             else if(amountp[O[n].getOwner().getPlayer()]==2) this.rent=1000;
             else if(amountp[O[n].getOwner().getPlayer()]==3) this.rent=2000;
